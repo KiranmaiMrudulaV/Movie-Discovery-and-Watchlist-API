ab01ebaae8148854494fde28c897b73e980df4b0
Kiranmai Mrudula Vardhiboyina <kiranmaimrudula.vardhiboyina@gmail.com>
2026-09-04 15:39:32 -0500

    Add Movie model

---
+++ src/MovieWatchlistApi/Models/Movie.cs
@@ -0,0 +1,22 @@
+namespace MovieWatchlistApi.Models;
+
+public class Movie
+{
+    public string Id { get; set; } = string.Empty;
+
+    public required string Title { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public List<string> Genres { get; set; } = new();
+
+    public int ReleaseYear { get; set; }
+
+    public int DurationMinutes { get; set; }
+
+    public double Rating { get; set; }
+
+    public List<string> CaptionLanguages { get; set; } = new();
+
+    public bool HasAudioDescription { get; set; }
+}
