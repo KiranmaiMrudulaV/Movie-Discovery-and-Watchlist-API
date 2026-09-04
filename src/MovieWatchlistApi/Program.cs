1551e849025b6ea57a6a57f9efb16f5275494a55
Kiranmai Mrudula Vardhiboyina <kiranmaimrudula.vardhiboyina@gmail.com>
2026-09-04 15:39:43 -0500

    Configure MongoDB connection via Options pattern and DI

--- src/MovieWatchlistApi/Program.cs
+++ src/MovieWatchlistApi/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using MovieWatchlistApi.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoDatabase>(serviceProvider =>
+{
+    var settings = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    var client = new MongoClient(settings.ConnectionString);
+    return client.GetDatabase(settings.DatabaseName);
+});
 
 var app = builder.Build();
 
